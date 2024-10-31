47193a003b4a8dcd4fbda07de1b96f6459a18657
Qiratsumra <saeedqirat43@gmail.com>
2024-10-31 12:05:47 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+"use client"
 import Image from "next/image";
 
-// Import Links
-import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 // Import Images:
 
 import qiratProfile from "../../public/qirat.linkdin.jpg";
@@ -11,6 +10,10 @@ import Header from "./header/page";
 import Footer from "./footer/page";
 
 export default function Home() {
+  let route = useRouter();
+  let navigate =(page:string)=>{
+    route.push(page)
+  }
   return (
     <main className="bg-black  text-red-200 max-sm:h-full max-sm:w-fit">
     <title>Qirat Saeed</title>
@@ -34,11 +37,16 @@ export default function Home() {
         <br />
         Javascript
           <br />
+
+          <button onClick={()=>navigate("https://github.com/Qiratsumra")} type="button" className="m-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">GitHub</button>
+
+          <button onClick={()=>navigate("https://www.linkedin.com/in/qirat-saeed-8048662b7/")} type="button" className=" p-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Linkedin</button>
           </p>
         </div>
         <div className="">
         <Image src={qiratProfile} alt="image" className="h-[400px] w-[400px] max-sm:h-[200px] max-sm:w-[200px]"/>
         </div>
+        
         </div>
     {/* Footer Section */}
     <Footer/>
